242153053714be7cf3fdd31a38ecd9eb12d1c8f8
Vipin Gopinath <vipin.vipin.g498@gmail.com>
2017-03-13 23:27:25 +0530

    Tag Clound v1

--- src/app/main.directive.ts
+++ src/app/main.directive.ts
@@ -24,21 +24,21 @@ this.el.nativeElement.style.transform='scale(1)';
 ngAfterViewInit() {
    var weight=this.el.nativeElement.getAttribute('data-weight');
 if(weight>=2){
-     this.el.nativeElement.style.fontSize="18px";
+     this.el.nativeElement.style.fontSize="16px";
     // this.el.nativeElement.style.fontWeight="400";
 }
     if(weight>=4){
-     this.el.nativeElement.style.fontSize="20px";
+     this.el.nativeElement.style.fontSize="24px";
         
     // this.el.nativeElement.style.fontWeight="600";
 }   
     if(weight>=6){
-     this.el.nativeElement.style.fontSize="24px";
+     this.el.nativeElement.style.fontSize="26px";
     
    this.el.nativeElement.style.color="#21ced4";    
 }
       if(weight>=10){
-     this.el.nativeElement.style.fontSize="28px";
+     this.el.nativeElement.style.fontSize="30px";
           
    this.el.nativeElement.style.color="#5fd0b5";
    //  this.el.nativeElement.style.fontWeight="900";
